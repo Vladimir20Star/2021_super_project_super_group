85bc97cf7297093d7696e22e9e135555aab8297d
Multiverse Mind <sanya.loginov03@yandex.ru>
2021-11-24 17:04:55 +0300

    Изменена логика запуска графики, более адекватное разделение на функции.

--- grafic.py
+++ grafic.py
@@ -4,25 +4,25 @@ from objects import Button
 
 class Sections:
     def __init__(self):
-        self.menu_finished = True
+        self.menu_finished = False
         self.game_finished = True
         self.profile_finished = True
+        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
         self.FPS = 60
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     def menu(self):
         pygame.display.update()
         clock = pygame.time.Clock()
-        self.menu_finished = False
-        game_button = Button(300, 400, 100, 100, 'red', self.game, 'game', 20, 30, 40, 'black')
-        profile_button = Button(600, 400, 100, 100, 'cyan', self.profile, 'game', 20, 30, 40, 'black')
-        exit_button = Button(900, 400, 100, 100, 'blue', self.menu_quit, 'exit', 20, 30, 40, 'black')
+        game_button = Button(300, 400, 100, 100, 'red', self.game_enter, 'game', 20, 30, 40, 'black')
+        profile_button = Button(600, 400, 100, 100, 'cyan', self.profile_enter, 'profile', 20, 30, 40, 'black')
+        exit_button = Button(900, 400, 100, 100, 'blue', self.total_exit, 'exit', 20, 30, 40, 'black')
         menu_button_list = [game_button, profile_button, exit_button]
         while not self.menu_finished:
             clock.tick(self.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.menu_finished = True
+                    self.total_exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     for button in menu_button_list:
                         button.click(event)
@@ -30,19 +30,17 @@ class Sections:
             for button in menu_button_list:
                 button.draw(self.screen)
             pygame.display.update()
-        pygame.quit()
 
     def game(self):
         pygame.display.update()
         clock = pygame.time.Clock()
-        self.game_finished = False
-        back_button = Button(0, 0, 100, 100, 'purple', self.game_quit, 'back', 20, 30, 40, 'black')
+        back_button = Button(0, 0, 100, 100, 'purple', self.menu_enter, 'back', 20, 30, 40, 'black')
         game_button_list = [back_button]
         while not self.game_finished:
             clock.tick(self.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.game_finished = True
+                    self.menu_enter()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     for button in game_button_list:
                         button.click(event)
@@ -55,13 +53,13 @@ class Sections:
         pygame.display.update()
         clock = pygame.time.Clock()
         self.profile_finished = False
-        back_button = Button(0, 0, 100, 100, 'orange', self.profile_quit, 'back', 20, 30, 40, 'black')
+        back_button = Button(0, 0, 100, 100, 'orange', self.menu_enter, 'back', 20, 30, 40, 'black')
         profile_button_list = [back_button]
         while not self.profile_finished:
             clock.tick(self.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    self.profile_finished = True
+                    self.menu_enter()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     for button in profile_button_list:
                         button.click(event)
@@ -70,15 +68,26 @@ class Sections:
                 button.draw(self.screen)
             pygame.display.update()
 
-    def menu_quit(self):
-        self.menu_finished = True
-
-    def game_quit(self):
+    def menu_enter(self):
+        self.menu_finished = False
         self.game_finished = True
-
-    def profile_quit(self):
         self.profile_finished = True
+        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
 
+    def game_enter(self):
+        self.menu_finished = True
+        self.game_finished = False
+        self.profile_finished = True
+        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
 
-sections = Sections()
-sections.menu()
+    def profile_enter(self):
+        self.menu_finished = True
+        self.game_finished = True
+        self.profile_finished = False
+        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
+
+    def total_exit(self):
+        self.menu_finished = True
+        self.game_finished = True
+        self.profile_finished = True
+        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
--- objects.py
+++ objects.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 pygame.font.init()
 
 
