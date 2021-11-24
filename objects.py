c052572e87ebd203e0a83ea6919ff6b253386c0f
Multiverse Mind <sanya.loginov03@yandex.ru>
2021-11-25 01:28:16 +0300

    Добавил объект фигуры, реализовал их движение.

--- objects.py
+++ objects.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 pygame.font.init()
 
@@ -26,3 +27,41 @@ class Button:
         font = pygame.font.Font(None, self.size)
         text = font.render(self.text, True, self.text_color)
         screen.blit(text, (self.x + self.x_text, self.y + self.y_text))
+
+
+class Figure(pygame.sprite.Sprite, Button):
+    def __init__(self, x, y, le, h, image_name, sprites, x_end, y_end):
+        super().__init__(sprites)
+        self.x = x
+        self.y = y
+        self.le = le
+        self.h = h
+        self.x_end = x_end
+        self.y_end = y_end
+        self.step_x = (self.x_end - self.x) / 20
+        self.step_y = (self.y_end - self.y) / 20
+        self.move_flag = False
+        self.function = self.start_move
+        fullname = os.path.join('img', image_name)
+        image = pygame.image.load(fullname).convert()
+        self.image = image
+        self.image = pygame.transform.scale(self.image, (self.le, self.h))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(self.x, self.y)
+
+    def start_move(self):
+        self.move_flag = True
+        color_key = self.image.get_at((0, 0))
+        self.image.set_colorkey(color_key)
+
+    def move(self):
+        if self.x == self.x_end and self.y == self.y_end:
+            self.move_flag = False
+        else:
+            self.x += self.step_x
+            self.y += self.step_y
+            if abs(self.x - self.x_end) < self.step_x or abs(self.y - self.y_end) < self.step_y:
+                self.x = self.x_end
+                self.y = self.y_end
+            self.rect.x = self.x
+            self.rect.y = self.y
