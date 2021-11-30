e0aff905678e30cd3c4da3d9b843c0697f3ac84a
Multiverse Mind <sanya.loginov03@yandex.ru>
2021-11-30 23:53:42 +0300

    В разделе игры реализована игра с учётом будущего изменения вызова функции игры через файл main.py.

--- grafic.py
+++ grafic.py
@@ -1,22 +1,50 @@
 import pygame
-from objects import Button, Figure
+import objects
+import time
+import os
+
+pygame.font.init()
+
+
+WINDOW_WIDTH = 1536
+WINDOW_HEIGHT = 864
+FIGURE_H_WIDTH = 117
+FIGURE_H_HEIGHT = 172
+FIGURE_B_WIDTH = FIGURE_B_HEIGHT = 200
+FIGURE_H_CEN_X = (WINDOW_WIDTH - FIGURE_H_WIDTH) // 2
+FIGURE_H_Y = WINDOW_HEIGHT - FIGURE_H_HEIGHT - 70
+FIGURE_H_END_X = (WINDOW_WIDTH - 117) // 2
+FIGURE_H_END_Y = WINDOW_HEIGHT // 2 + 10
+FIGURE_B_X = (WINDOW_WIDTH - FIGURE_B_WIDTH) // 2
+FIGURE_B_Y = WINDOW_HEIGHT // 2 - FIGURE_B_HEIGHT - 10
 
 
 class Sections:
     def __init__(self):
         self.menu_finished = False
         self.game_finished = True
+        self.game_again = False
         self.profile_finished = True
-        self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
+        self.flag_list = [self.menu_finished, self.profile_finished]
         self.FPS = 60
+        self.countdown = 0
+        self.font = pygame.font.Font(None, 500)
+        self.text = self.font.render('', True, 'black')
+        self.text_width = self.text.get_width()
+        self.text_height = self.text.get_height()
+        self.click_flag = False
+        self.figures_active = True
+        self.countdown_time = time.time()
+        self.click_time = time.time()
+        self.human_figure = 0
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 
     def menu(self):
         pygame.display.update()
         clock = pygame.time.Clock()
-        game_button = Button(300, 400, 100, 100, 'red', self.game_enter, 'game', 20, 30, 40, 'black')
-        profile_button = Button(600, 400, 100, 100, 'cyan', self.profile_enter, 'profile', 20, 30, 40, 'black')
-        exit_button = Button(900, 400, 100, 100, 'blue', self.total_exit, 'exit', 20, 30, 40, 'black')
+        game_button = objects.Button(300, 400, 100, 100, 'red', self.game_enter, 'game', 20, 30, 40, 'black')
+        profile_button = objects.Button(600, 400, 100, 100, 'cyan', self.profile_enter, 'profile', 20, 30, 40, 'black')
+        exit_button = objects.Button(900, 400, 100, 100, 'blue', self.total_exit, 'exit', 20, 30, 40, 'black')
         menu_button_list = [game_button, profile_button, exit_button]
         while not self.menu_finished:
             clock.tick(self.FPS)
@@ -31,40 +59,101 @@ class Sections:
                 button.draw(self.screen)
             pygame.display.update()
 
-    def game(self):
+    def game(self, bot_figure, victory_count, draw_count, defeat_count):
         pygame.display.update()
         clock = pygame.time.Clock()
         game_sprites = pygame.sprite.Group()
-        back_button = Button(0, 0, 100, 100, 'purple', self.menu_enter, 'back', 20, 30, 40, 'black')
-        human_rock = Figure(540, 600, 117, 172, 'Камень человека.gif', game_sprites, 710, 400)
-        human_scissors = Figure(710, 600, 117, 172, 'Ножницы человека.gif', game_sprites, 710, 400)
-        human_paper = Figure(880, 600, 117, 172, 'Бумага человека.gif', game_sprites, 710, 400)
+        back_button = objects.Button(0, 0, 100, 100, 'purple', self.menu_enter, 'back', 20, 30, 40, 'black')
+        human_rock = objects.Figure(FIGURE_H_CEN_X - 170, FIGURE_H_Y, FIGURE_H_WIDTH, FIGURE_H_HEIGHT,
+                                    'Камень человека.gif', game_sprites, FIGURE_H_END_X, FIGURE_H_END_Y)
+        human_scissors = objects.Figure(FIGURE_H_CEN_X, FIGURE_H_Y, FIGURE_H_WIDTH, FIGURE_H_HEIGHT,
+                                        'Ножницы человека.gif', game_sprites, FIGURE_H_END_X, FIGURE_H_END_Y)
+        human_paper = objects.Figure(FIGURE_H_CEN_X + 170, FIGURE_H_Y, FIGURE_H_WIDTH, FIGURE_H_HEIGHT,
+                                     'Бумага человека.gif', game_sprites, FIGURE_H_END_X, FIGURE_H_END_Y)
+        start_button = objects.Button(0, 500, 100, 100, 'purple', self.game_restart, 'start', 20, 30, 40, 'black')
         figure_list = [human_rock, human_scissors, human_paper]
-        game_button_list = [back_button]
-        while not self.game_finished:
+        game_button_list = [back_button, start_button]
+        self.click_flag = False
+        self.game_again = False
+        self.figures_active = True
+        self.human_figure = 0
+        self.font = pygame.font.Font(None, 500)
+        self.text = self.font.render('', True, 'black')
+        result = 0
+        self.countdown_tick()
+        if bot_figure == 1:
+            fullname = os.path.join('img', 'Камень бота.gif')
+        elif bot_figure == 2:
+            fullname = os.path.join('img', 'Ножницы бота.gif')
+        else:
+            fullname = os.path.join('img', 'Бумага бота.gif')
+        image = pygame.image.load(fullname).convert()
+        color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+        bot_figure_image = pygame.transform.scale(image, (FIGURE_B_WIDTH, FIGURE_B_HEIGHT))
+        while not self.game_finished and not self.game_again:
             clock.tick(self.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.menu_enter()
-                elif event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN and self.click_flag:
                     for button in game_button_list:
                         button.click(event)
                     for figure in figure_list:
-                        figure.click(event)
+                        if figure.active:
+                            figure.click(event)
+                            if figure.move_flag:
+                                self.human_figure = figure_list.index(figure) + 1
+                                for fig in figure_list:
+                                    fig.active = False
+                                break
             for figure in figure_list:
                 if figure.move_flag:
                     figure.move()
+            if self.countdown > 0:
+                if time.time() - self.countdown_time > 1:
+                    self.countdown_tick()
+                    self.countdown_time = time.time()
+            elif not self.click_flag:
+                self.text = self.font.render('', True, 'black')
+                self.text_width = self.text.get_width()
+                self.text_height = self.text.get_height()
+                self.click_flag = True
+                self.click_time = time.time()
+            if self.click_flag and time.time() - self.click_time > 0.5 and self.figures_active:
+                for figure in figure_list:
+                    figure.active = False
+                self.font = pygame.font.Font(None, 350)
+                self.text = self.font.render('', True, 'black')
+                if self.human_figure == 0:
+                    self.text = self.font.render('Не сыграно!', True, 'black')
+                elif (self.human_figure, bot_figure) in [(1, 3), (2, 1), (3, 2)]:
+                    self.text = self.font.render('Поражение!', True, 'black')
+                    result = -1
+                elif (self.human_figure, bot_figure) in [(1, 2), (2, 3), (3, 1)]:
+                    self.text = self.font.render('Победа!', True, 'black')
+                    result = 1
+                else:
+                    self.text = self.font.render('Ничья!', True, 'black')
+                self.text_width = self.text.get_width()
+                self.text_height = self.text.get_height()
+                self.figures_active = False
             self.screen.fill('pink')
+            if self.click_flag and time.time() - self.click_time > 0.25:
+                self.screen.blit(bot_figure_image, (FIGURE_B_X, FIGURE_B_Y))
             for button in game_button_list:
                 button.draw(self.screen)
             game_sprites.draw(self.screen)
+            self.screen.blit(self.text, ((WINDOW_WIDTH - self.text_width) // 2,
+                                         (WINDOW_HEIGHT - self.text_height) // 2))
             pygame.display.update()
+        return self.human_figure, result
 
     def profile(self):
         pygame.display.update()
         clock = pygame.time.Clock()
         self.profile_finished = False
-        back_button = Button(0, 0, 100, 100, 'orange', self.menu_enter, 'back', 20, 30, 40, 'black')
+        back_button = objects.Button(0, 0, 100, 100, 'orange', self.menu_enter, 'back', 20, 30, 40, 'black')
         profile_button_list = [back_button]
         while not self.profile_finished:
             clock.tick(self.FPS)
@@ -88,6 +177,7 @@ class Sections:
     def game_enter(self):
         self.menu_finished = True
         self.game_finished = False
+        self.game_again = False
         self.profile_finished = True
         self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
 
@@ -102,3 +192,17 @@ class Sections:
         self.game_finished = True
         self.profile_finished = True
         self.flag_list = [self.menu_finished, self.game_finished, self.profile_finished]
+
+    def game_restart(self):
+        self.game_again = True
+
+    def countdown_tick(self):
+        if self.countdown == 0:
+            self.countdown = 5
+            self.countdown_time = time.time()
+        else:
+            self.countdown -= 1
+        if self.countdown > 0:
+            self.text = self.font.render(str(self.countdown), True, 'black')
+        self.text_width = self.text.get_width()
+        self.text_height = self.text.get_height()
--- objects.py
+++ objects.py
@@ -4,23 +4,28 @@ import os
 pygame.font.init()
 
 
-class Button:
-    def __init__(self, x, y, le, h, color, function, text, size, x_text, y_text, text_color):
+class BaseButton:
+    def __init__(self, x, y, le, h, function):
         self.x = x
-        self.le = le
         self.y = y
+        self.le = le
         self.h = h
+        self.function = function
+
+    def click(self, click_event):
+        if self.x < click_event.pos[0] < self.x + self.le and self.y < click_event.pos[1] < self.y + self.h:
+            self.function()
+
+
+class Button(BaseButton):
+    def __init__(self, x, y, le, h, color, function, text, size, x_text, y_text, text_color):
+        super().__init__(x, y, le, h, function)
         self.color = color
         self.text = text
         self.size = size
         self.x_text = x_text
         self.y_text = y_text
         self.text_color = text_color
-        self.function = function
-
-    def click(self, click_event):
-        if self.x < click_event.pos[0] < self.x + self.le and self.y < click_event.pos[1] < self.y + self.h:
-            self.function()
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.le, self.h))
@@ -29,19 +34,17 @@ class Button:
         screen.blit(text, (self.x + self.x_text, self.y + self.y_text))
 
 
-class Figure(pygame.sprite.Sprite, Button):
+class Figure(pygame.sprite.Sprite, BaseButton):
     def __init__(self, x, y, le, h, image_name, sprites, x_end, y_end):
-        super().__init__(sprites)
-        self.x = x
-        self.y = y
-        self.le = le
-        self.h = h
+        pygame.sprite.Sprite.__init__(self, sprites)
+        BaseButton.__init__(self, x, y, le, h, self.start_move)
         self.x_end = x_end
         self.y_end = y_end
-        self.step_x = (self.x_end - self.x) / 20
-        self.step_y = (self.y_end - self.y) / 20
+        self.step_count = 20
+        self.step_x = (self.x_end - self.x) / self.step_count
+        self.step_y = (self.y_end - self.y) / self.step_count
         self.move_flag = False
-        self.function = self.start_move
+        self.active = True
         fullname = os.path.join('img', image_name)
         image = pygame.image.load(fullname).convert()
         self.image = image
@@ -60,7 +63,7 @@ class Figure(pygame.sprite.Sprite, Button):
         else:
             self.x += self.step_x
             self.y += self.step_y
-            if abs(self.x - self.x_end) < self.step_x or abs(self.y - self.y_end) < self.step_y:
+            if abs(self.x - self.x_end) < abs(self.step_x) or abs(self.y - self.y_end) < abs(self.step_y):
                 self.x = self.x_end
                 self.y = self.y_end
             self.rect.x = self.x
