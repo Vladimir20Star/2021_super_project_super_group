48731ddd240ee61728bd1937d7c30be9bbf9e0f6
FrolovKirill <kkbhbr808645>
2021-12-01 21:40:58 +0300

    Переписал нейросеть через матрицы и изменил функцию активации.

--- rock-paper-scissors neural network.py
+++ rock-paper-scissors neural network.py
@@ -1,4 +1,4 @@
-from numpy import exp, array, dot, zeros, random
+import numpy as np
 import matplotlib.pyplot as plt
 
 """
@@ -9,7 +9,13 @@ import matplotlib.pyplot as plt
 
 
 def activation(x):
-    return 0.5 + 3 / (1 + exp(-x))
+    return 0.5 + 1.5 / (1 + np.exp(-x - 2.5)) + 1.5 / (1 + np.exp(-x + 2.5))
+
+
+def activation_derivative(x):
+    var_1 = np.exp(-x - 2.5)
+    var_2 = np.exp(-x + 2.5)
+    return 1.5 * var_1 / (1 + var_1) ** 2 + 1.5 * var_2 / (1 + var_2) ** 2
 
 
 def round_to(x):
@@ -26,90 +32,112 @@ def learning():
     y1 = []
     y2 = []
 
-    bias = 1
     e = 0.5
     a = 0.5
 
     education_string = [
         "313133211132112223312313122333213213231323131331231131232133322113131313221133213333123313123213331331331332223321",
         "3223122132111333113221132321322132232223311233122313333323321121332223311332312333311331132231131123322133211332311"]
-    training_int1 = array([int(i) for i in education_string[0]])  # Костыль, надо исправить
-    training_int2 = array([int(i) for i in education_string[1]])  # Костыль, надо исправить
-    weights_1 = random.uniform(-3, 3, (3, 6))
-    weights_2 = random.uniform(-3, 3, 4)
 
-    grad_w1 = zeros((3, 6))
+    training_int1 = np.array([int(i) for i in education_string[0]])  # костыль
+    training_int2 = np.array([int(i) for i in education_string[1]])  # костыль
+    weights_1 = np.random.uniform(-1, 1, (3, 5))
+    weights_2 = np.random.uniform(-1, 1, (1, 3))
+    b1 = np.random.uniform(-1, 1, (3, 1))
+    b2 = np.random.uniform(-1, 1)
 
-    pre_delta_w1 = zeros((3, 6))
-    pre_delta_w2 = zeros(4)
+    pre_delta_w1 = np.zeros((3, 5))
 
-    h = array([0, 0, 0, bias], dtype=float)
+    pre_delta_w2 = np.zeros((1, 3))
+
+    pre_delta_b1 = np.zeros((3, 1))
+
+    pre_delta_b2 = 0
 
     for j in range(20000):
         for i in range(len(training_int1) - 5):
-            input_data = array([training_int1[i], training_int1[i + 1], training_int1[i + 2],
-                                training_int1[i + 3], training_int1[i + 4], bias])
-            for k in range(3):
-                h[k] = activation(dot(weights_1[k], input_data))
-            output = activation(dot(h, weights_2))
+            input_data = np.array([training_int1[i], training_int1[i + 1], training_int1[i + 2],
+                                   training_int1[i + 3], training_int1[i + 4]]).reshape((-1, 1)).reshape((-1, 1))
+            inp_h = np.dot(weights_1, input_data) + b1
+            h = activation(inp_h)
+            s = np.dot(weights_2, h) + b2
+            prediction = activation(s)
+            grad_prediction = activation_derivative(s) * (training_int1[i + 5] - prediction)
 
-            delta_out = (output - 0.5) * (7 / 6 - output / 3) * (training_int1[i + 5] - output)
-            delta_h = (h - 0.5) * (7 / 6 - h / 3) * (weights_2 * delta_out)
-            grad_w2 = h * delta_out
-            delta_w2 = e * grad_w2 + a * pre_delta_w2
-            weights_2 += delta_w2
-            pre_delta_w2 = delta_w2
+            grad_w2 = (grad_prediction * h).T
+            delta_w2 = grad_w2 * e + pre_delta_w2 * a
+            grad_b2 = grad_prediction
+            delta_b2 = grad_b2 * e + pre_delta_b2 * a
 
-            for k in range(3):
-                grad_w1[k] = input_data * delta_h[k]
-            delta_w1 = e * grad_w1 + pre_delta_w1 * a
+            grad_h = activation_derivative(inp_h)
+            grad_w1 = grad_prediction * (np.dot(input_data, (weights_2 * grad_h.T))).T
+            delta_w1 = grad_w1 * e + pre_delta_w1 * a
+            grad_b1 = grad_prediction * (weights_2.reshape((-1, 1)) * grad_h)
+            delta_b1 = grad_b1 * e + pre_delta_b1 * a
+
             weights_1 += delta_w1
+            weights_2 += delta_w2
+            b1 += delta_b1
+            b2 += delta_b2
+
             pre_delta_w1 = delta_w1
-        x.append(j)
-        y1.append(weights_2[0])
-        y2.append(weights_2[3])
+            pre_delta_w2 = delta_w2
+            pre_delta_b1 = delta_b1
+            pre_delta_b2 = delta_b2
+
         for i in range(len(training_int2) - 5):
-            input_data = array([training_int2[i], training_int2[i + 1], training_int2[i + 2],
-                                training_int2[i + 3], training_int2[i + 4], bias])
-            for k in range(3):
-                h[k] = activation(dot(weights_1[k], input_data))
-            output = activation(dot(h, weights_2))
+            input_data = np.array([training_int2[i], training_int2[i + 1], training_int2[i + 2],
+                                   training_int2[i + 3], training_int2[i + 4]]).reshape((-1, 1)).reshape((-1, 1))
+            inp_h = np.dot(weights_1, input_data) + b1
+            h = activation(inp_h)
+            s = np.dot(weights_2, h) + b2
+            prediction = activation(s)
+            grad_prediction = activation_derivative(s) * (training_int2[i + 5] - prediction)
 
-            delta_out = (output - 0.5) * (7 / 6 - output / 3) * (training_int2[i + 5] - output)
-            delta_h = (h - 0.5) * (7 / 6 - h / 3) * (weights_2 * delta_out)
-            grad_w2 = h * delta_out
-            delta_w2 = e * grad_w2 + a * pre_delta_w2
-            weights_2 += delta_w2
-            pre_delta_w2 = delta_w2
+            grad_w2 = (grad_prediction * h).T
+            delta_w2 = grad_w2 * e + pre_delta_w2 * a
+            grad_b2 = grad_prediction
+            delta_b2 = grad_b2 * e + pre_delta_b2 * a
 
-            for k in range(3):
-                grad_w1[k] = input_data * delta_h[k]
-            delta_w1 = e * grad_w1 + pre_delta_w1 * a
+            grad_h = activation_derivative(inp_h)
+            grad_w1 = grad_prediction * (np.dot(input_data, (weights_2 * grad_h.T))).T
+            delta_w1 = grad_w1 * e + pre_delta_w1 * a
+            grad_b1 = grad_prediction * (weights_2.reshape((-1, 1)) * grad_h)
+            delta_b1 = grad_b1 * e + pre_delta_b1 * a
+
             weights_1 += delta_w1
-            pre_delta_w1 = delta_w1
-        x.append(j)
-        y1.append(weights_2[0])
-        y2.append(weights_2[3])
-        if j > 19700:
-            print(weights_2[0], weights_2[3])
+            weights_2 += delta_w2
+            b1 += delta_b1
+            b2 += delta_b2
 
-    print(weights_1, weights_2)
+            pre_delta_w1 = delta_w1
+            pre_delta_w2 = delta_w2
+            pre_delta_b1 = delta_b1
+            pre_delta_b2 = delta_b2
+
+        x.append(j)
+        y1.append(weights_2[0][0])
+        y2.append(weights_2[0][2])
+        if j > 19700:
+            print(weights_2[0][0], weights_2[0][2])
+
+    print(weights_1, weights_2, b1, b2)
     plt.plot(x, y1)
     plt.plot(x, y2)
     plt.show()
 
 
 def test():
-    #input_string = "10010101011111111001110000001011101000111100111110100000111111111111110000000000110101010101100110"
-    #input_int = array([int(i) for i in input_string])
-    bias = 1
     player = bot = draw = 0
-    weights_1 = array([[-0.65324095, 0.08473624, -2.75593248, -2.94977803, -4.19452158, -3.96927836],
-                       [-4.97214371, 0.76401118, -6.67989925, 0.61377894, -8.08062782, -0.3201619],
-                       [-2.28546697, -3.94500862, 6.88317406, -12.64563122, -11.80378841, 1.59190351]])
-    weights_2 = array([-2.46409999, 0.81389463, 1.76516013, -0.63625594])
-    h = array([0, 0, 0, bias], dtype=float)
-    input_data = []
+    weights_1 = np.array([[-21.13164301, -1.37054036, -16.86976406, 29.30538071, -34.47326956],
+                          [48.60277746, -40.69339409, 23.70495276, -50.88796299, -48.15155281],
+                          [15.25138701, -14.91345246, -21.40183576, -13.08390208, 10.26623152]])
+    weights_2 = np.array([[1.69228692, 1.68394246, 1.9765986]])
+    b1 = np.array([[10.03311103],
+                   [16.75359252],
+                   [-11.22556562]])
+    b2 = [[-2.93831495]]
+    input_data = np.zeros((5, 1))
 
     for i in range(5):
         a = input()
@@ -119,8 +147,8 @@ def test():
             player_move = 2
         else:
             player_move = 3
-        input_data.append(player_move)
-        output = random.randint(1, 3)
+        input_data[i][0] = player_move
+        output = np.random.randint(1, 3)
         if output == 1:
             bot_move = "k"
         elif output == 2:
@@ -146,7 +174,6 @@ def test():
             else:
                 print(bot_move, "Победил игрок")
 
-    input_data.append(bias)
     for i in range(50):
         player_data = input()
         if player_data == "k":
@@ -156,46 +183,44 @@ def test():
         else:
             player_move = 3
 
-        for k in range(3):
-            h[k] = activation(dot(weights_1[k], input_data))
-        output = activation(dot(h, weights_2))
-        output = round_to(output)
+        h = activation(np.dot(weights_1, input_data) + b1)
+        prediction = activation(np.dot(weights_2, h) + b2)
+        prediction = round_to(prediction)
 
-        if output == 1:
+        if prediction == 1:
             bot_move = "k"
-        elif output == 2:
+        elif prediction == 2:
             bot_move = "b"
         else:
             bot_move = "n"
 
-        if player_move == output:
+        if player_move == prediction:
             draw += 1
             print(bot_move, "Ничья!")
         elif player_move == 2:
-            if output > player_move:
+            if prediction > player_move:
                 bot += 1
                 print(bot_move, "Победил бот")
             else:
                 player += 1
                 print(bot_move, "Победил игрок")
         elif player_move == 1:
-            if output == 2:
+            if prediction == 2:
                 bot += 1
                 print(bot_move, "Победил бот")
             else:
                 player += 1
                 print(bot_move, "Победил игрок")
         else:
-            if output == 1:
+            if prediction == 1:
                 bot += 1
                 print(bot_move, "Победил бот")
             else:
                 player += 1
                 print(bot_move, "Победил игрок")
         for j in range(4):
-            print(input_data)
-            input_data[j] = input_data[j + 1]
-        input_data[4] = player_move
+            input_data[j][0] = input_data[j + 1][0]
+        input_data[4][0] = player_move
     print(bot / 50, player / 50, draw / 50)
 
 
