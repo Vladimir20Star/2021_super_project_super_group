79999fb2265d8dda48ab62985718b00357587dbf
FrolovKirill <kkbhbr808645>
2021-12-05 22:03:33 +0300

    Тестовая версия работы по сети.

--- client.py
+++ client.py
@@ -2,7 +2,8 @@ import socket
 
 sock = socket.socket()
 sock.connect(('localhost', 16363))
-sock.send(155)
+string1 = "hello, world!"
+sock.send(string1.encode("hello, world!"))
 
 data = sock.recv(1024)
 sock.close()
